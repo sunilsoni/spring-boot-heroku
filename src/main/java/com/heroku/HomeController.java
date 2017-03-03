34aa875103f4a4528db1a142f422cf2bf9b2af08
Sunil Soni <sunilsoni@live.in>
2017-03-03 12:04:19 +0530

    DB removed

--- src/main/java/com/heroku/HomeController.java
+++ src/main/java/com/heroku/HomeController.java
@@ -1,14 +1,8 @@
 
 package com.heroku;
 
-import javax.validation.Valid;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,28 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomeController {
 
-    private RecordRepository repository;
-   
-    @Autowired
-    public HomeController(RecordRepository repository) {
-        this.repository = repository;
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public String home(ModelMap model) {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String home(ModelMap model) {
-        //List<Record> records = repository.findAll();
-        //model.addAttribute("records", records);
-        model.addAttribute("insertRecord", new Record());
-        return "home";
-    }
+		return "home";
+	}
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String insertData(ModelMap model, 
-                             @ModelAttribute("insertRecord") @Valid Record record,
-                             BindingResult result) {
-        if (!result.hasErrors()) {
-            repository.save(record);
-        }
-        return home(model);
-    }
 }
--- src/main/java/com/heroku/RecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.heroku;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository  
-public interface RecordRepository extends JpaRepository<Record, Long> {
-}
