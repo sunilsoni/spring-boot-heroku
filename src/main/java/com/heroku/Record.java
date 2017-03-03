302938f170c75e89da607a6b6f75abb55d8b2482
Sunil Soni <sunilsoni@live.in>
2017-03-03 10:43:01 +0530

    first commit

---
+++ src/main/java/com/heroku/Record.java
@@ -0,0 +1,41 @@
+package com.heroku;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+   
+
+	@NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+    
+}
